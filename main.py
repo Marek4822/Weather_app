d95d68f0790dd7b048a73061a660db08c0a71d6d
Marek Bastian <136629186+Marek4822@users.noreply.github.com>
2023-08-03 18:10:39 +0200

    add names of the days of the week
    add names of the days of the week

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from cred import *
+from credentials import *
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 import requests
-
 
 web_path = f"https://www.messenger.com/t/{threadID}"
 driver = webdriver.Chrome()
 driver.get(web_path)
 sleep(1)
 
+def get_day_name():
+    dt = datetime.now()
+    day_name = dt.strftime('%A')
+    return day_name
+
+day_name = get_day_name()
+
 def get_weather():
     city_name = 'Gdańsk'
     url = f'http://api.weatherapi.com/v1/forecast.json?key={weather_api}&q={city_name}&days=1&aqi=yes&alerts=no'
     request = requests.get(url)
     data = request.json()
-
-    date = data['forecast']['forecastday'][0]['date']
+    # date = data['forecast']['forecastday'][0]['date']
     max_temp = data['forecast']['forecastday'][0]['day']['maxtemp_c']
     min_temp = data['forecast']['forecastday'][0]['day']['mintemp_c']
     condition = data['forecast']['forecastday'][0]['day']['condition']['text']
 
-    return date, max_temp, min_temp, condition
+    return max_temp, min_temp, condition
 
-weather_parameter = get_weather()
-
+max_temp, min_temp, condition = get_weather()
 
 def login():
     driver.find_element(By.XPATH, "//button[@title='Allow all cookies' and @type='submit']").click()
@@ -34,20 +39,22 @@ def login():
     driver.find_element(By.ID, "loginbutton").click()
 
 
-def create_messege(weather_parameter):
-    message = f'{weather_parameter}'
+def create_messege(max_temp, min_temp, condition, day_name):
+    message = f'{max_temp}, {min_temp}, {condition}, {day_name}'
 
     return message
 
-message = create_messege(weather_parameter)
+message = create_messege(max_temp, min_temp, condition, day_name)
 
 print(message)
 
 
 def send_messege(message):
-    driver.find_element(By.XPATH, "//div[contains(@class, 'xzsf02u')]").send_keys(message)
-    driver.find_element(By.XPATH, "//div[contains(@class, 'xzsf02u')]").send_keys(Keys.RETURN)
-
+    send = driver.find_element(By.XPATH, "//div[contains(@class, 'xzsf02u x1a2a7pz x1n2onr6')]")
+    send.send_keys(message)
+    enter = driver.find_element(By.XPATH, "//div[contains(@class, 'xzsf02u x1a2a7pz x1n2onr6')]")
+    enter.send_keys(Keys.RETURN)
+# xzsf02u x1a2a7pz x1n2onr6 x14wi4xw x1iyjqo2 x1gh3ibb xisnujt xeuugli x1odjw0f notranslate
 
 def main():
     try:
@@ -56,13 +63,9 @@ def main():
         send_messege(message)
     except Exception as e:
         print(f'Error occurred: {e}')
-        sleep(1)
-
     # Wait for user input before exiting the script
     input("Press Enter to close the browser...")
 
 
 if __name__ == "__main__":
     main()
-
-
