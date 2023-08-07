d8f30a90e29ae564f5f12620193e3a0c73cb44b1
Marek Bastian <136629186+Marek4822@users.noreply.github.com>
2023-08-07 18:04:47 +0200

    adding chatgpt api
    Adding an API from Chatgpt to generate messages about weather

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from credentials import *
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import requests
+import openai
+import unicodedata
 
 web_path = f"https://www.messenger.com/t/{threadID}"
 driver = webdriver.Chrome()
@@ -27,10 +29,28 @@ def get_weather():
     max_temp = data['forecast']['forecastday'][0]['day']['maxtemp_c']
     min_temp = data['forecast']['forecastday'][0]['day']['mintemp_c']
     condition = data['forecast']['forecastday'][0]['day']['condition']['text']
+    rain_chance = data['forecast']['forecastday'][0]['day']['daily_chance_of_rain']
 
-    return max_temp, min_temp, condition
+    return max_temp, min_temp, condition, rain_chance
 
-max_temp, min_temp, condition = get_weather()
+max_temp, min_temp, condition , rain_chance = get_weather()
+
+def chatgpt_api():
+    openai.api_key = openai_api
+
+    prompt = f'your character traits is funny, nice, full of positive energy. Your taks will be to generate funny, positive with emoji morning welcome message including paraments like temperature=from {max_temp}°C to {min_temp}°C, weather description={condition}, chanse of rain={rain_chance}, name of the week={day_name}. At the end of a messege give me short fun fact. You will reply only with a welcome message that includes the paraments I mention above and end your message at the end of fun fact and dont write anything else. Use emojis only supported by unicode'
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+    )
+    gpt_answer = completion.choices[0].message.content
+
+    return gpt_answer
+
+gpt_answer = chatgpt_api()
 
 def login():
     driver.find_element(By.XPATH, "//button[@title='Allow all cookies' and @type='submit']").click()
@@ -38,16 +58,12 @@ def login():
     driver.find_element(By.ID, "pass").send_keys(pas)
     driver.find_element(By.ID, "loginbutton").click()
 
+def convert_messege(text):
+    normalized_text = unicodedata.normalize('NFC', text)
+    char_bmp = ''.join(char for char in normalized_text if ord(char) <= 0xFFFF)
+    return char_bmp
 
-def create_messege(max_temp, min_temp, condition, day_name):
-    message = f'{max_temp}, {min_temp}, {condition}, {day_name}'
-
-    return message
-
-message = create_messege(max_temp, min_temp, condition, day_name)
-
-print(message)
-
+message = convert_messege(gpt_answer)
 
 def send_messege(message):
     send = driver.find_element(By.XPATH, "//div[contains(@class, 'xzsf02u x1a2a7pz x1n2onr6')]")
@@ -61,11 +77,10 @@ def main():
         login()
         sleep(5)
         send_messege(message)
+        sleep(1)
+        driver.close()
     except Exception as e:
         print(f'Error occurred: {e}')
-    # Wait for user input before exiting the script
-    input("Press Enter to close the browser...")
-
 
 if __name__ == "__main__":
     main()
